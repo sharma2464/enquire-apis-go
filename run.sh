510e4871083d3599394a0ca29d9c073617e1b606
Manish S <manish@medpiper.com>
2022-04-05 12:31:18 +0530

    Added automatic running for replit

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 # gin run main.go --appPort 5000 --port 3000 --excludeDir ./frontend
+echo "$date | Building source code to binary -> `main`"
 go build main.go
+echo "$date | Build Complete"
+echo "$date | Running `main`..."
 ./main
